fc8b9376d9be38c35d212edc42df58bf27e1b54e
simonsobs <simonsobs@example.com>
2022-04-20 17:18:14 +0200

    Test clients for boresight motion and scanning

--- agents/acu/test_clients/attic/linscan_template.py
+++ agents/acu/test_clients/attic/linscan_template.py
@@ -2,18 +2,18 @@ import numpy as np
 import time
 import ocs
 from ocs import client_t, site_config
-from ocs.matched_client import MatchedClient
+from ocs.ocs_client import OCSClient
 import sys
 
-def upload_track(azpts, el, azvel, acc, ntimes, scantype):
+def upload_track(azpts, el, azvel, acc, ntimes, azonly):
      
-    acu_client = MatchedClient('acu1')
+    acu_client = OCSClient('acu-emmy')
     acu_client.go_to.start(az=azpts[0], el=el, wait=1)
     acu_client.go_to.wait()
     acu_client.stop_and_clear.start()
     acu_client.stop_and_clear.wait()
-    acu_client.run_specified_scan.start(azpts=azpts, el=el, azvel=azvel, acc=acc, ntimes=ntimes, scantype=scantype)
-    acu_client.run_specified_scan.wait()
+    acu_client.linear_turnaround_scan.start(azpts=azpts, el=el, azvel=azvel, acc=acc, ntimes=ntimes, azonly=azonly)
+    acu_client.linear_turnaround_scan.wait()
 
 if __name__ == '__main__':
-    upload_track((140., 160.), 40., 1., 4, 2, 'linear_turnaround')
+    upload_track((140., 160.), 40., 1., 4, 5, False)
